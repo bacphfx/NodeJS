e953306565b07fc0be37c62034ad859a3813eade
Pham Ho Bac <bacphfx16330@funix.edu.vn>
2022-10-08 00:45:20 +0700

    add product to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const { restart } = require("nodemon");
 const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -37,6 +38,14 @@ exports.getCart = (req, res, next) => {
     path: "/cart",
     pageTitle: "Your Cart",
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
 };
 
 exports.getOrders = (req, res, next) => {
