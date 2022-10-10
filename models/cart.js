a16ae0ecf2ef0cd63053fd29b6b1af0cd06b593d
Pham Ho Bac <bacphfx16330@funix.edu.vn>
2022-10-10 22:38:19 +0700

    show cart

--- models/cart.js
+++ models/cart.js
@@ -52,4 +52,15 @@ module.exports = class Cart {
       });
     });
   }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(nul);
+      } else {
+        cb(cart);
+      }
+    });
+  }
 };
