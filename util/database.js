d99fa66bd630302b8f40b807cd9155148fdad53b
Pham Ho Bac <bacphfx16330@funix.edu.vn>
2022-10-17 01:35:03 +0700

    setup sql

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "node-complete",
+  password: "BunDau2210",
+});
+
+module.exports = pool.promise()
