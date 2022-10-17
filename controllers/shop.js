3ec39deab24cad23905f2c815447dcba3dc15839
Pham Ho Bac <bacphfx16330@funix.edu.vn>
2022-10-18 01:25:29 +0700

    install sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,7 +5,6 @@ const Cart = require("../models/cart");
 exports.getProducts = (req, res, next) => {
   Product.fetchAll()
     .then(([rows, fieldData]) => {
-      console.log(rows);
       res.render("shop/product-list", {
         prods: rows,
         pageTitle: "All Products",
@@ -19,13 +18,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
-    res.render("shop/product-detail", {
-      product: product,
-      pageTitle: product.title,
-      path: "/products",
-    });
-  });
+  Product.findById(prodId)
+    .then(([product]) => {
+      res.render("shop/product-detail", {
+        product: product[0],
+        pageTitle: product.title,
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
