bec3fb15eb0221dae9fe9e73f2cd77a63c0f5273
Pham Ho Bac <bacphfx16330@funix.edu.vn>
2023-01-03 16:26:09 +0700

    test 2

--- models/products.js
+++ models/products.js
@@ -26,6 +26,7 @@ module.exports = class Products {
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
