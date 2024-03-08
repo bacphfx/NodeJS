0f6eb62feb24538878debaf140c1645c255fc6ec
unknown <bacphfx16330@funix.edu.vn>
2024-03-08 11:27:57 +0700

    sign up

---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { body } = require("express-validator/check");
+const User = require("../models/user");
+const authController = require("../controllers/auth");
+
+const router = express.Router();
+
+router.put(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((val, { req }) => {
+        return User.findOne({ email: val }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject("Email already exists!");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password").trim().isLength({ min: 5 }),
+    body("name").trim().not().isEmpty(),
+  ],
+  authController.signup
+);
+
+module.exports = router;
