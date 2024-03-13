233952481ee2541e54db06b590c9838e7eaabc6a
Pham Ho Bac <bacphfx16330@funix.edu.vn>
2024-03-14 00:46:55 +0700

    update post io

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -44,6 +44,8 @@ class Feed extends Component {
     socket.on("posts", (data) => {
       if (data.action === "create") {
         this.addPost(data.post);
+      } else if (data.action === "update") {
+        this.updatePost(data.post);
       }
     });
   }
@@ -52,12 +54,29 @@ class Feed extends Component {
     this.setState((prevState) => {
       const updatedPosts = [...prevState.posts];
       if (prevState.postPage === 1) {
-        updatedPosts.pop();
+        if (prevState.posts.length >= 2) {
+          updatedPosts.pop();
+        }
         updatedPosts.unshift(post);
       }
       return {
         posts: updatedPosts,
         totalPosts: prevState.totalPosts + 1,
+      };
+    });
+  };
+
+  updatePost = (post) => {
+    this.setState((prevState) => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex(
+        (p) => p._id === post._id
+      );
+      if (updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post;
+      }
+      return {
+        posts: updatedPosts,
       };
     });
   };
@@ -182,15 +201,7 @@ class Feed extends Component {
           createdAt: resData.post.createdAt,
         };
         this.setState((prevState) => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              (p) => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          }
           return {
-            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false,
